fce78b2e6e5da5a0cff96aecc3d306883f7e85a4
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-09-02 00:27:27 -0700

    refactor text file to js

--- src/template.js
+++ src/template.js
@@ -1,31 +1,33 @@
-# Markdown Previewer
-
+const templateText = `# Markdown Previewer\n
+\n
 Welcome to the markdown previewer! This previewer will show you how
-markdown renders.
+markdown renders.\n
+\n
+## Examples of what you can do in markdown\n
+Make a list\n
+* bullets\n
+  * with different levels\n
+\n
+Or numbered...\n
+1. One\n
+2. Two\n
+\n
+Use **bold text** to **emphasize a point!**\n
+\n
+Annotate \`code in-line\` or in a block...\n
+\n
+\`\`\`\n
+def hello_markdown():\n
+    print('Wowza!')\n
+\`\`\`\n
+\n
+![Fireworks](https://images.unsplash.com/photo-1533230408708-8f9f91d1235a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80)\n
+\n
+Blockquotes provide a nice way to reference other ideas.\n
+>Positive thinking will let you do everything better than negative thinking will.\n
+>\n
+>-Zig Ziglar\n
+\n
+I hope that you enjoy this tool, feel free to [checkout my other projects on github](https://github.com/willbeaumont)!\n`;
 
-## Examples of what you can do in markdown
-Make a list
-* bullets
-  * with different levels
-
-Or numbered...
-1. One
-2. Two
-
-Use **bold text** to **emphasize a point!**
-
-Annotate `code in-line` or in a block...
-
-```
-def hello_markdown():
-    print('Wowza!')
-```
-
-![Fireworks](https://images.unsplash.com/photo-1533230408708-8f9f91d1235a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8&w=1000&q=80)
-
-Blockquotes provide a nice way to reference other ideas.
->Positive thinking will let you do everything better than negative thinking will.
->
->-Zig Ziglar
-
-I hope that you enjoy this tool, feel free to [checkout my other projects on github](https://github.com/willbeaumont)!
+export default templateText;
