19b82209f033fb923d97bf89b2b1ea70d8787e95
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-09-02 00:28:07 -0700

    show template text, render template as html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import './App.css';
+import { marked } from 'marked';
+import React from 'react';
+import templateText from './template.js';
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorText: templateText
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.editorText}</div>
+        <Rendered text={this.state.editorText}/>
+      </div>
+    )
+  }
+}
+
+function Rendered(props) {
+  const html = marked.parse(props.text);
+  return (
+    <div dangerouslySetInnerHTML={{__html: html}}/>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App-header"/>
+    <Editor />
   );
 }
 
