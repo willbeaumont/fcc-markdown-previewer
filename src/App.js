ef1936a63dc8a3640c0f15ce2e683571b9f537c9
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-09-06 23:34:41 -0700

    add option to read newline as br tag

--- src/App.js
+++ src/App.js
@@ -12,19 +12,18 @@ function Editor(props) {
 }
 
 function Preview(props) {
-  const dirtyHtml = marked.parse(props.text);
+  const dirtyHtml = marked.parse(props.text, { breaks: true });
 
   return (
     <div id="preview" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(dirtyHtml)}}/>
   )
 }
 
-
 function App() {
   const [text, setText] = useState(templateText)
 
   return (
-    <div>
+    <div className="App-header">
       <Editor text={text} onChange={event => setText(event.target.value)}/>
       <Preview text={text}/>
     </div>
