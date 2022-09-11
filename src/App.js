e94d7f4e6bb0c4af38c2f5ee0cbfddd2f24f5e93
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-09-10 23:05:28 -0700

    add card div for styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import templateText from './template.js';
 
 function Editor(props) {
   return (
+    <div className="Card">
       <textarea id="editor" value={props.text} onChange={props.onChange}/>
+    </div>
   )
 }
 
@@ -15,7 +17,7 @@ function Preview(props) {
   const dirtyHtml = marked.parse(props.text, { breaks: true });
 
   return (
-    <div id="preview" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(dirtyHtml)}}/>
+    <div className="Card" id="preview" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(dirtyHtml)}}/>
   )
 }
 
@@ -23,9 +25,14 @@ function App() {
   const [text, setText] = useState(templateText)
 
   return (
-    <div className="App-header">
-      <Preview text={text}/>
-      <Editor text={text} onChange={event => setText(event.target.value)}/>
+    <div className="App-content">
+      <div className="App-header">
+        <h1>&larr; Preview</h1><h1>Editor &rarr;</h1>
+      </div>
+      <div className="App-cards">
+        <Preview text={text}/>
+        <Editor text={text} onChange={event => setText(event.target.value)}/>
+      </div>
     </div>
   );
 }
