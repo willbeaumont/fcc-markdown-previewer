3bbc6f7496c41677888fc8bdca5b56b5d2ad7f02
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-09-11 10:28:30 -0700

    remove css styling for tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { marked } from 'marked';
 import DOMPurify from 'dompurify';
 import React, { useState } from 'react';
@@ -7,7 +6,7 @@ import templateText from './template.js';
 
 function Editor(props) {
   return (
-    <div className="Card">
+    <div className="">
       <textarea id="editor" value={props.text} onChange={props.onChange}/>
     </div>
   )
@@ -17,7 +16,7 @@ function Preview(props) {
   const dirtyHtml = marked.parse(props.text, { breaks: true });
 
   return (
-    <div className="Card" id="preview" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(dirtyHtml)}}/>
+    <div className="" id="preview" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(dirtyHtml)}}/>
   )
 }
 
@@ -25,11 +24,11 @@ function App() {
   const [text, setText] = useState(templateText)
 
   return (
-    <div className="App-content">
-      <div className="App-header">
+    <div className="">
+      <div className="">
         <h1>&larr; Preview</h1><h1>Editor &rarr;</h1>
       </div>
-      <div className="App-cards">
+      <div className="">
         <Preview text={text}/>
         <Editor text={text} onChange={event => setText(event.target.value)}/>
       </div>
