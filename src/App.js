a90ff895f5171f5ab8c5e39aa37f4384073f3363
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2022-09-11 15:58:52 -0700

    fix render location to remained focused during edit, add styling

--- src/App.js
+++ src/App.js
@@ -3,35 +3,42 @@ import DOMPurify from 'dompurify';
 import React, { useState } from 'react';
 import templateText from './template.js';
 
-function Card({content: Content}) {
+const cardCss = "p-6 w-full h-full overflow-auto mx-auto bg-slate-200 rounded-xl shadow-lg items-center";
+
+function Editor(props) {
   return (
-    <div className="p-6 w-1/2 mx-auto bg-slate-200 rounded-xl shadow-lg items-center">
-      <Content />
+    <div className={cardCss}>
+      <textarea className="form-textarea w-full h-full" id="editor" value={props.text} onChange={props.onChange}/>
+    </div>
+  )
+}
+
+function Preview(props) {
+  const dirtyHtml = marked.parse(props.text, { breaks: true });
+
+  return (
+    <div className= {cardCss}>
+      <div className="Card" id="preview" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(dirtyHtml)}}/>
     </div>
   )
 }
 
 function App() {
   const [text, setText] = useState(templateText)
-
-  const Editor = () => {
-    return <textarea className="form-textarea w-full h-full " id="editor" value={text} onChange={event => setText(event.target.value)}/>;
-  };
-
-  const Preview = () => {
-    const dirtyHtml = marked.parse(text, { breaks: true });
-    return <div id="preview" dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(dirtyHtml)}}/>;
-  };
+  const colCss = "flex flex-col items-center h-1/2 lg:w-1/2 lg:h-full"
 
   return (
     <div className="bg-slate-400">
       <div className="h-screen">
-        <div className="flex items-center">
-          <h1>&larr; Preview</h1><h1>Editor &rarr;</h1>
-        </div>
-        <div className="flex flex-row p-6 space-x-4">
-          <Card content={Preview} />
-          <Card content={Editor} />
+        <div className="flex flex-col lg:flex-row p-6 lg:space-x-4 h-full">
+          <div className={colCss}>
+            <h1>Editor</h1>
+            <Editor text={text} onChange={event => setText(event.target.value)}/>
+          </div>
+          <div className={colCss}>
+            <h1>Preview</h1>
+            <Preview text={text}/>
+          </div>
         </div>
       </div>
     </div>
